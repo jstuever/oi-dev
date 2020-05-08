1e43064e0350ada122c32876ea11cc772c17906b
Jeremiah Stuever <jstuever@redhat.com>
2020-05-08 11:52:52 -0700

    Added scripts/watchbootstrap.sh to assist with viewing the logs on the bootstrap host

---
+++ scripts/watchbootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ASSETDIR="${1-assets}"
+
+while true; do
+  if [ -f ${ASSETDIR}/metadata.json ]; then
+    if [ "$(jq '.aws' ${ASSETDIR}/metadata.json)" != "null" ]; then
+      IP=$(jq '.resources[] | select(.module == "module.bootstrap") | select(.type == "aws_instance") | select(.name == "bootstrap") | .instances[0].attributes.public_ip' ${ASSETDIR}/terraform.tfstate | tr -d "\"")
+    elif [ "$(jq '.gcp' ${ASSETDIR}/metadata.json)" != "null" ]; then
+      IP=$(jq '.resources[] | select(.module == "module.bootstrap") | select(.type == "google_compute_address") | select(.name == "bootstrap") | .instances[0].attributes.address' ${ASSETDIR}/terraform.tfstate | tr -d "\"")
+    else
+      echo "No supported platforms found in ${ASSETDIR}/metadata.json"
+    fi;
+  else
+    echo "${ASSETDIR}/metadata.json not found"
+    IP=""
+  fi
+
+  if [ -n "${IP}" ];
+  then
+    ssh -i ${OPT_PRIVATE_KEY:-'~/.ssh/openshift-dev.pem'} \
+      -o ConnectTimeout=5 \
+      -o StrictHostKeyChecking=no \
+      -o PasswordAuthentication=no \
+      -o UserKnownHostsFile=/dev/null \
+      core@${IP} \
+      'journalctl -b -f -u release-image.service -u bootkube.service'
+  fi
+  sleep 10
+done;
