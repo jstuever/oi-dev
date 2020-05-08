4df24f65feb6dbd5c943b788ae1094771d5013bd
Jeremiah Stuever <jstuever@redhat.com>
2020-05-08 12:02:50 -0700

    Added scripts/watchcluster.sh to assist with monitoring a cluster during bootstrap

---
+++ scripts/watchcluster.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export KUBECONFIG=${1-assets}/auth/kubeconfig
+
+watch -n 5 '
+  oc get clusterversion --no-headers \
+  && oc get events -o json | jq -r ".items[] | select(.type != \"Normal\") | select(.reason != \"Rebooted\") | .lastTimestamp + \" \" + .type + \" \" + .reason + \": \" + .message" | tail -n 5
+  oc get nodes;
+  oc get csr --no-headers | grep "Pending";
+  oc get clusteroperator | grep -v "True        False         False";
+'
