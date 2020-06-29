20988fa3968496b32fb644971d480c2958f048d9
Jeremiah Stuever <jstuever@redhat.com>
2020-06-29 16:26:12 -0700

    watchcluster: Use quotes on watch command, and use ASSETDIR more clearly

--- scripts/watchcluster.sh
+++ scripts/watchcluster.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-export KUBECONFIG=${1-assets}/auth/kubeconfig
+ASSETDIR="${1-assets}"
+export KUBECONFIG=${ASSETDIR}/auth/kubeconfig
 
-watch -n 5 '
+watch -n 10 "
   oc get clusterversion --no-headers \
-  && oc get events -o json | jq -r ".items[] | select(.type != \"Normal\") | select(.reason != \"Rebooted\") | .lastTimestamp + \" \" + .type + \" \" + .reason + \": \" + .message" | tail -n 5
+  && oc get events -o json | jq -r \".items[] | select(.type != \\\"Normal\\\") | select(.reason != \\\"Rebooted\\\") | .lastTimestamp + \\\" \\\" + .type + \\\" \\\" + .reason + \\\": \\\" + .message\" | tail -n 5
   oc get nodes;
-  oc get csr --no-headers | grep "Pending";
-  oc get clusteroperator | grep -v "True        False         False";
-'
+  oc get csr --no-headers --sort-by=\".metadata.creationTimestamp\"| grep \"Pending\";
+  oc get clusteroperator | grep -v \"True        False         False\";
+"
