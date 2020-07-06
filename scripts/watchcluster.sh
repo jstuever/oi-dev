1f27125094d4f2046d66c53b5fc480e5a40ef610
Jeremiah Stuever <jstuever@redhat.com>
2020-07-06 15:03:46 -0700

    watchcluster: wrap script with --command

--- scripts/watchcluster.sh
+++ scripts/watchcluster.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 
+# Determine if we are watching or executing.
+if [[ "$1" == "--execute" ]]; then
+  _EXECUTE=1
+  shift
+fi
+
+# Input assetdir $1, or default to assets.
 ASSETDIR="${1-assets}"
 export KUBECONFIG=${ASSETDIR}/auth/kubeconfig
 
-watch -n 10 "
-  if [ -s ${KUBECONFIG} ]; then
-    oc get clusterversion --no-headers \
-    && oc get events -o json | jq -r \".items[] | select(.type != \\\"Normal\\\") | select(.reason != \\\"Rebooted\\\") | .lastTimestamp + \\\" \\\" + .type + \\\" \\\" + .reason + \\\": \\\" + .message\" | tail -n 5
-    oc get nodes;
-    oc get csr --no-headers --sort-by=\".metadata.creationTimestamp\"| grep \"Pending\";
-    oc get clusteroperator | grep -v \"True        False         False\";
-  else
-    echo \"${KUBECONFIG} not found.\"
-  fi
-"
+if [[ ${_EXECUTE-0} == 0 ]]; then
+  # Watch while we re-run the script with --execute.
+  watch -n 10 $0 --execute $*
+  exit
+fi
+
+# Now we --execute
+if [ -s ${KUBECONFIG} ]; then
+  oc get clusterversion --no-headers \
+    && oc get events -o json \
+    | jq -r ".items[] \
+      | select(.type != \"Normal\") \
+      | select(.reason != \"Rebooted\") \
+      | .lastTimestamp + \" \" + .type + \" \" + .reason + \": \" + .message" \
+   | tail -n 5
+  oc get nodes
+  oc get csr --no-headers --sort-by=".metadata.creationTimestamp" | grep "Pending"
+  oc get clusteroperator | grep -v "True        False         False"
+else
+  echo "${KUBECONFIG} not found."
+fi
