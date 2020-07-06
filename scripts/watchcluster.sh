b072c6a8f99620db9107414ed40456246c68bee8
Jeremiah Stuever <jstuever@redhat.com>
2020-07-06 13:16:22 -0700

    watchcluster: check for kubeconfig before using it

--- scripts/watchcluster.sh
+++ scripts/watchcluster.sh
@@ -4,9 +4,13 @@ ASSETDIR="${1-assets}"
 export KUBECONFIG=${ASSETDIR}/auth/kubeconfig
 
 watch -n 10 "
-  oc get clusterversion --no-headers \
-  && oc get events -o json | jq -r \".items[] | select(.type != \\\"Normal\\\") | select(.reason != \\\"Rebooted\\\") | .lastTimestamp + \\\" \\\" + .type + \\\" \\\" + .reason + \\\": \\\" + .message\" | tail -n 5
-  oc get nodes;
-  oc get csr --no-headers --sort-by=\".metadata.creationTimestamp\"| grep \"Pending\";
-  oc get clusteroperator | grep -v \"True        False         False\";
+  if [ -s ${KUBECONFIG} ]; then
+    oc get clusterversion --no-headers \
+    && oc get events -o json | jq -r \".items[] | select(.type != \\\"Normal\\\") | select(.reason != \\\"Rebooted\\\") | .lastTimestamp + \\\" \\\" + .type + \\\" \\\" + .reason + \\\": \\\" + .message\" | tail -n 5
+    oc get nodes;
+    oc get csr --no-headers --sort-by=\".metadata.creationTimestamp\"| grep \"Pending\";
+    oc get clusteroperator | grep -v \"True        False         False\";
+  else
+    echo \"${KUBECONFIG} not found.\"
+  fi
 "
