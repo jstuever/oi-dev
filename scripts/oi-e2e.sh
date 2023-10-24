b8f1e126a461e33cd47d304ebcdef82089de7049
Jeremiah Stuever <jstuever@redhat.com>
2023-10-24 14:21:51 -0700

    oi-e2e: enable Azure platform

--- scripts/oi-e2e.sh
+++ scripts/oi-e2e.sh
@@ -6,7 +6,7 @@
 #
 # Requirements:
 # * ~/.gcp/osServiceAccount.json
-# * assets/auth/kubeconfig
+# * KUBECONFIG (default: assets/auth/kubeconfig)
 # * relevant versions of the following binaries (first up in $PATH)
 #   * kubectl
 #   * oc
@@ -15,31 +15,18 @@ set -uvo pipefail
 
 ASSETDIR="${1-assets}"
 
-TEST_SUITE="${TEST_SUITE-openshift/conformance/parallel}"
 TEST_BINARY="${TEST_BINARY-openshift-tests}"
+TEST_SUITE="${TEST_SUITE-openshift/conformance/parallel}"
 
+export AZURE_AUTH_LOCATION="${HOME}/.azure/osServicePrincipal.json"
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.gcp/osServiceAccount.json"
+export KUBECONFIG=${KUBECONFIG-${ASSETDIR}/auth/kubeconfig}
 
 # Configure KUBECONFIG
-if [[ ! -s "${ASSETDIR}/auth/kubeconfig" ]]; then
-  echo "${ASSETDIR}/auth/kubeconfig empty or not found"
+if [[ ! -s "${KUBECONFIG}" ]]; then
+  echo "${KUBECONFIG} empty or not found"
   exit 1
 fi
-export KUBECONFIG="${ASSETDIR}/auth/kubeconfig"
-
-# Configure TEST_PRIVIDER based on platform
-PLATFORM="$(oc get -o jsonpath='{.status.platform}' infrastructure cluster)"
-case "${PLATFORM}" in
-GCP)
-  PROJECT_NAME="$(oc get -o jsonpath='{.status.platformStatus.gcp.projectID}' infrastructure cluster)"
-  REGION="$(oc get -o jsonpath='{.status.platformStatus.gcp.region}' infrastructure cluster)"
-  TEST_PROVIDER="{\"type\":\"gce\",\"region\":\"${REGION}\",\"multizone\": true,\"multimaster\":true,\"projectid\":\"${PROJECT_NAME}\"}"
-  ;;
-*)
-  echo "Unsupported platform: ${PLATFORM}"
-  exit 1
-  ;;
-esac
 
 # Create directory to store test results
 TEST_DIR="${ASSETDIR}/e2e/$(date -u +%Y%m%d-%H%M%S)"
@@ -47,7 +34,6 @@ mkdir -p "${TEST_DIR}"
 
 # Run test suite
 "${TEST_BINARY}" run "${TEST_SUITE}" \
-    --provider "${TEST_PROVIDER}" \
     -o "${TEST_DIR}/e2e.log" \
     --junit-dir "${TEST_DIR}"
 ret="$?"
